078f61c24ed3e9cb44cd4e9ae2ac71f88e9447b4
Bharat Rajan <brajan@ua.edu>
2017-12-08 01:23:54 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * @description - Check if n is integer
+ * @utility
+ * @param {integer} n - value to be validated for integer
+ * @returns boolean : interger or not
+ */
+var isNInteger = function(n) {
+    return (n === parseInt(n))
+}
+
+/**
+ * @description - Computes factorial for integer n
+ * @utility
+ * @param {integer} n - n for which factorial is calculated
+ * @returns string : Factorial of n. Or error message
+ */
+var getFactorial = (function() {
+    //Closure to safeguard cacheArr
+    var cacheArr = [new BigNumber("1"), new BigNumber("1")];
+
+    return function(n) {
+        //Validation for integer
+        if (!isNInteger(n)) {
+            return "Invalid input";
+        }
+
+        var result = cacheArr[n];
+        if (typeof result === "object") {
+            //Already computed value
+            return result.toString();
+        } else {
+            //Compute from biggest factorial available
+            var iter = cacheArr.length - 1;
+            result = cacheArr[iter]
+            while (iter <= n) {
+                cacheArr[iter] = result = result.multiply(iter.toString());
+                iter++;
+            }
+            return result.toString();
+        }
+
+    }
+
+})();
